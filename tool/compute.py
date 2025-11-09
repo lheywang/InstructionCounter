3d59a10dbb9e52a5d6387f21135802252fca4b15
lheywang <leonard.heywang@proton.me>
2025-11-09 21:33:24 +0100

    Finished the computation step with a LOT of infos about the executed program

--- tool/compute.py
+++ tool/compute.py
@@ -49,12 +49,14 @@ def count_instructions(data: dict) -> dict:
                 functions_instr[name][instr] = 0
 
             # Add to the variables
-            instr_counts[instr] += data["blocks"][key][instr]
+            instr_counts[instr] += (
+                data["blocks"][key][instr] * data["calls"][key]["count"]
+            )
             functions_instr[name][instr] += data["blocks"][key][instr]
 
         # Ponderate with the right call numbers
         block_instr = funct_instr * data["calls"][key]["count"]
-        
+
         # Append for later :
         if name not in function_counts:
             function_counts[name] = 0
@@ -62,11 +64,21 @@ def count_instructions(data: dict) -> dict:
 
         total_instr += block_instr
 
+    # Count unique instructions
+    uniques = 0
+    for values in functions_instr.values():
+        for count in values.values():
+            uniques += count
+
     # Sort and store the largest functions calls
-    function_counts = sorted(function_counts.items(), key=lambda x: x[1], reverse=True)
+    function_counts = dict(
+        sorted(function_counts.items(), key=lambda x: x[1], reverse=True)
+    )
+
     data["output"]["instructions"] = total_instr
+    data["output"]["unique_instructions"] = uniques
     data["output"]["instructions_counts"] = instr_counts
-    data["output"]["func_calls_instr"] = dict(function_counts)
+    data["output"]["func_calls_instr"] = function_counts
     data["output"]["func_used_instr"] = functions_instr
     return data
 
@@ -112,10 +124,13 @@ def count_cycles(data: dict) -> dict:
         total_cycles += block_cycle
 
     # Sort and store the largest functions calls
-    function_counts = sorted(function_counts.items(), key=lambda x: x[1], reverse=True)
+    function_counts = dict(
+        sorted(function_counts.items(), key=lambda x: x[1], reverse=True)
+    )
+
     data["output"]["cycles"] = total_cycles
     data["output"]["cycles_count"] = cycles_counts
-    data["output"]["func_calls_cycles"] = dict(function_counts)
+    data["output"]["func_calls_cycles"] = function_counts
     return data
 
 
@@ -129,18 +144,73 @@ def compute_cpi(data: dict) -> dict:
     # Create output target
     func_cpi = dict()
 
-    # Compute the per function CPI : 
+    # Compute the per function CPI :
     for func in data["output"]["func_calls_cycles"].keys():
-        func_cpi[func] = data["output"]["func_calls_cycles"][func] / data["output"]["func_calls_instr"][func]
+        func_cpi[func] = (
+            data["output"]["func_calls_cycles"][func]
+            / data["output"]["func_calls_instr"][func]
+        )
 
     # Sorting the func cpi list
     func_cpi = dict(sorted(func_cpi.items(), key=lambda x: x[1], reverse=True))
 
     # Store outputs
     data["output"]["cpi"] = cpi
+    data["output"]["ipc"] = 1 / cpi
     data["output"]["func_cpi"] = func_cpi
-        
+
     return data
+
+
+def get_density(instr_dict: dict, cfg_dict: dict) -> dict:
+    """
+    Return densities of instruction from a defined set of instructions, depending on their type.
+    Used to identify the bottlenecks for each sets.
+
+    In order, it returns :
+        - jumps
+        - conditions
+        - standard
+        - muldiv
+        - memory
+        - specials (CSR)
+    """
+
+    # Prepare outputs and keys
+    instr_types = [0, 0, 0, 0, 0, 0]
+    instr_percents = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    names = ["jumps", "conditions", "arithmetic", "muldiv", "memory", "special"]
+
+    # Filter the instruction, by their types
+    for instr in instr_dict.keys():
+        for index, name in enumerate(names):
+            if instr.upper() in cfg_dict[name].keys():
+                instr_types[index] += instr_dict[instr]
+
+    # Compute the proportions
+    total = sum(instr_types)
+    for index, value in enumerate(instr_types):
+        instr_percents[index] = value * 100 / total
+
+    # Compute some regroupements
+    branch_count = instr_types[0] + instr_types[1]
+    arithmetic_count = instr_types[2] + instr_types[3]
+
+    branch_percent = branch_count * 100 / total
+    arithmetic_percent = arithmetic_count * 100 / total
+
+    # set the output data
+    outdict = dict()
+    outdict["percents"] = dict(zip(names, instr_percents))
+    outdict["counts"] = dict(zip(names, instr_types))
+
+    outdict["branch_densities"] = branch_percent
+    outdict["arithmetic_densities"] = arithmetic_percent
+    outdict["memory_densities"] = instr_percents[4]
+    outdict["special_densities"] = instr_percents[5]
+
+    # Return the data
+    return outdict
 
 
 def compute_densities(data: dict) -> dict:
@@ -148,6 +218,43 @@ def compute_densities(data: dict) -> dict:
     Compute various instructions densities, to get a view of the most used ones,
     and their proportions.
     """
+
+    # First, load the config file
+    cfg = dict()
+    with open("config/instructions.toml", "rb") as file:
+        cfg = tomllib.load(file)
+
+    # First, get the whole density namings
+    densities = get_density(data["output"]["instructions_counts"], cfg)
+    func_densities = dict()
+
+    # Then, compute the per functions densities
+    for func, values in data["output"]["func_used_instr"].items():
+        func_densities[func] = get_density(values, cfg)
+
+    # Output data
+    data["output"]["densities"] = densities
+    data["output"]["func_densities"] = func_densities
+
+    # Return
+    return data
+
+
+def compute_averages(data: dict) -> dict:
+    """
+    Compute some averages to be added, that can give us some infos about
+    dfferent behaviors
+    """
+    func_nb = len(data["output"]["func_used_instr"])
+
+    # Count the functions numbers
+    data["output"]["averages"] = dict()
+    data["output"]["averages"]["func_number"] = func_nb
+
+    data["output"]["averages"]["instr_per_func"] = (
+        data["output"]["unique_instructions"] / func_nb
+    )
+
     return data
 
 
@@ -199,8 +306,9 @@ if __name__ == "__main__":
     data = count_cycles(data)
 
     # Perform more advanced maths :
-    data = compute_cpi(data)
     data = compute_densities(data)
+    data = compute_averages(data)
+    data = compute_cpi(data)
 
     # Create the output file :
     write_json(output, data["output"])
